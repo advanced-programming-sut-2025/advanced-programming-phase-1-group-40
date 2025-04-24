8ae6bf316eebe47d41527816f1d27cd5defbefac
amiryusofabdi <amiryusofabdiabdi1@gmail.com>
2025-04-24 18:37:36 +0330

    Maven update

---
+++ src/main/java/org/example/view/SignUpMenu.java
@@ -0,0 +1,14 @@
+package org.example.view;
+
+import org.example.controller.Game.*;
+import org.example.controller.User.*;
+import java.util.*;
+
+public class SignUpMenu implements AppMenu {
+
+    @Override
+    public void getInput(String input, Scanner scanner){
+
+    }
+
+}
