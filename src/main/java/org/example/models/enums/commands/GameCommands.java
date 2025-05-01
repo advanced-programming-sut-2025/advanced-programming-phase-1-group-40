ecebdb71bf6fb419d17f035c9fef05ef88849a28
amiryusofabdi <amiryusofabdi1@gmail.com>
2025-05-01 11:18:37 +0330

    interaction with others added(till trade)

--- src/main/java/org/example/models/enums/commands/GameCommands.java
+++ src/main/java/org/example/models/enums/commands/GameCommands.java
@@ -20,14 +20,50 @@ public enum GameCommands implements Command{
 
     ///  COMMAND HAYE DAMDARI
 
-    BUILD("\\s+build\\s+-a\\s+(?<buildingName>.+)\\s+-l\\s+(?<x>\\S+)\\s+(?<y>\\S+)\\s+"),
-    BUY_ANIMAL("\\s+buy\\s+animal\\s+-a\\s+(?<animalName>.+)\\s+-n\\s+(?<name>.+)\\s+"),
-    PET("\\s+pet\\s+-n\\s+(?<name>.+)\\s+"),
-    CHEAT_SET_FRIENDSHIP("\\s+cheat\\s+set\\s+friendship\\s+-n\\s+(?<animalName>.+)\\s+-c\\s+(?<amount>.+)\\s+"),
-    SHOW_ANIMAL_INFO("\\s+animals\\s+"),
-    SHEPHERD_ANIMALS("\\s+shepherd\\s+animals\\s+-n\\s+(?<animalName>.+)\\s+-l\\s+(?<x>\\S+)\\s+(?<y>\\S+)\\s+"),
-    FEED_HAY("\\s+feed\\s+hay\\s+-n\\s+(?<animalName>.+)\\s+"),
-    SHOW_UNCOLLECTED_PRODUCTS("\\s+produces\\s+");
+
+    BUILD("\\s*build\\s+-a\\s+(?<buildingName>.+)\\s+-l\\s+(?<x>\\S+)\\s+(?<y>\\S+)\\s*"),
+    BUY_ANIMAL("\\s*buy\\s+animal\\s+-a\\s+(?<animalName>.+)\\s+-n\\s+(?<name>.+)\\s*"),
+    PET("\\s*pet\\s+-n\\s+(?<name>.+)\\s*"),
+    CHEAT_SET_FRIENDSHIP("\\s*cheat\\s+set\\s+friendship\\s+-n\\s+(?<animalName>.+)\\s+-c\\s+(?<amount>.+)\\s*"),
+    SHOW_ANIMAL_INFO("\\s*animals\\s*"),
+    SHEPHERD_ANIMALS("\\s*shepherd\\s+animals\\s+-n\\s+(?<animalName>.+)\\s+-l\\s+(?<x>\\S+)\\s+(?<y>\\S+)\\s*"),
+    FEED_HAY("\\s*feed\\s+hay\\s+-n\\s+(?<animalName>.+)\\s*"),
+    SHOW_UNCOLLECTED_PRODUCTS("\\s+produces\\s*"),
+    COLLECT_PRODUCTS("\\s*collect\\s+produce\\s+n-\\s+(?<name>.+)\\s*"),
+    SELL_ANIMAL("\\s*sell\\s+animal\\s+n-\\s+(?<name>.+)\\s*"),
+    FISHING("\\s*fishing\\s+p-\\s+(?<fishingPole>.+)\\s*"),
+
+
+
+    ///  COMMAND HAYE ARTISAN
+
+
+    ARTISAN_USE("\\s*artisan\\s+use\\s+(?<artisanName>.+)\\s+(?<itemName>.+)\\s*"),
+    ARTISAN_GET("\\s*artisan\\s+get\\s+(?<artisanName>.+)\\s*"),
+    SHOW_ALL_PRODUCTS_IN_STORE("\\s*show\\s+all\\s+products\\s*"),
+    SHOW_AVAILABLE_PRODUCTS_IN_STORE("\\s+show\\s+all\\s+available\\s+products\\s*"),
+    PURCHASE_ITEM("\\s*purchase\\s+(?<productName>.+)\\s+-n\\s+(?<count>.+)\\s*"),
+    CHEAT_ADD_MONEY("\\s*cheat\\s+add\\s+(?<count>.+)\\s+dollars\\s*"),
+    SELL_ITEM("\\s*sell\\s+(?<productName>.+)\\s+-n\\s+(?<count>.+)\\s*"),
+
+
+    /// COMMAND HAYE TAAMOLAT VA RAVABET BA BAZIKONAYE DIGE
+
+
+    SHOW_FRIENDSHIP("\\s*friendship\\s*"),
+    TALK_WITH_OTHERS("\\s*talk\\s+-u\\s+(?<username>.+)\\s+-m\\s+(?<message>.+)\\s*"),
+    SHOW_TALK_HISTORY("\\s*talk\\s+history\\s+-u\\s+(?<username>.+)\\s*"),
+    GIVE_GIFT_TO_OTHERS("\\s*gift\\s+-u\\s+(?<username>.+)\\s+-i\\s+(?<item>.+)\\s+-a\\s+(?<amount>.+)\\s*"),
+    SHOW_RECEIVED_GIFTS("\\s*gift\\s+list\\s*"),
+    RATE_GIFT("\\s*gift\\s+rate\\s+-i\\s+(?<giftNumber>.+)\\s+-r\\s+(?<rate>.+)\\s*"),
+    GIFT_HISTORY_WITH_USER("\\s*gift\\s+history\\s+-u\\s+(?<username>.+)\\s*"),
+    HUG_USER("\\s*hug\\s+-u\\s+(?<username>.+)\\s*"),
+    FLOWER_USER("\\s*flower\\s+-u\\s+(?<username>.+)\\s*"),
+    ASK_MARRIAGE("\\s*ask\\s+marriage\\s+-u\\s+(?<username>.+)\\s+-r\\s+(?<ring>.+)\\s*"),
+    RESPOND_TO_MARRIAGE("\\s*respond\\s+(-accept|-reject)\\s+-u\\s+(?<username>.+)\\s*"),
+    START_TRADE("\\s*start\\s+trade\\s*");
+
+
 
 
     private final String regexString;
