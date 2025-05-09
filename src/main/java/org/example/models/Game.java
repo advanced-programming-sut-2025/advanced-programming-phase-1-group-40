a7a7ff10ccc06b0f840d00537ef9ca465f858963
Dorsa <zabetidor@gmail.com>
2025-05-09 19:18:10 +0330

    Game

--- src/main/java/org/example/models/Game.java
+++ src/main/java/org/example/models/Game.java
@@ -16,5 +16,31 @@ public class    Game {
         this.farms = new ArrayList<>();
         this.shops = new ArrayList<>();
     }
+    public ArrayList<Farm> getFarms() {
+        return farms;
+    }
 
+    public ArrayList<org.example.models.Shop> getShops() {
+        return shops;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public int getCropGrowthRate() {
+        return cropGrowthRate;
+    }
+
+    public boolean isAutomaticIrrigation() {
+        return automaticIrrigation;
+    }
+
+    public int getEnergyUsageRate() {
+        return energyUsageRate;
+    }
+
+    public boolean isPossibilityOfThor() {
+        return possibilityOfThor;
+    }
 }
