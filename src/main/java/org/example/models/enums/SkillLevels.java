e1642e7e2fc494b4a7eaf1adb41bc00a7e9a915d
amiryusofabdi <amiryusofabdiabdi1@gmail.com>
2025-05-10 17:23:35 +0330

    backpack upadated

--- src/main/java/org/example/models/enums/SkillLevels.java
+++ src/main/java/org/example/models/enums/SkillLevels.java
@@ -2,8 +2,21 @@ package org.example.models.enums;
 
 
 public enum SkillLevels {
-    LEVEL_ZERO,
-    LEVEL_ONE, 
-    LEVEL_TWO,
-    LEVEL_THREE;
+
+    LEVEL_ZERO(0),
+    LEVEL_ONE(1),
+    LEVEL_TWO(2),
+    LEVEL_THREE(3);
+
+
+    private final int level;
+
+    SkillLevels(int level) {
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
 }
