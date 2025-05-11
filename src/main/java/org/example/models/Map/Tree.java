819a5688b6d5a9e4a6c6bb4cd41f1cb0be279690
amiryusofabdi <amiryusofabdiabdi1@gmail.com>
2025-05-11 11:04:35 +0330

    tree update

--- src/main/java/org/example/models/Map/Tree.java
+++ src/main/java/org/example/models/Map/Tree.java
@@ -1,4 +1,44 @@
 package org.example.models.Map;
 
+import org.example.models.Position;
+import org.example.models.enums.types.FruitType;
+import org.example.models.enums.types.TreeType;
+
 public class Tree {
+
+    private final TreeType treeType;
+    private final Position position;
+    private boolean isFullyGrown;
+    private boolean isBurnt;
+
+    public Tree(TreeType treeType, Position position) {
+        this.treeType = treeType;
+        this.position = position;
+        this.isFullyGrown = false;
+        this.isBurnt = false;
+    }
+
+    public void setFullyGrown(boolean fullyGrown) {
+        isFullyGrown = fullyGrown;
+    }
+
+    public void setBurnt(boolean burnt) {
+        isBurnt = burnt;
+    }
+
+    public boolean isFullyGrown() {
+        return isFullyGrown;
+    }
+
+    public boolean isBurnt() {
+        return isBurnt;
+    }
+
+    public TreeType getTreeType() {
+        return treeType;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
 }
