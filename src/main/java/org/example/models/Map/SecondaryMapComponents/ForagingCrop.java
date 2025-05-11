2f049fb526c9902eb9aee770a1587a820435ffd1
amiryusofabdi <amiryusofabdiabdi1@gmail.com>
2025-05-11 17:11:37 +0330

    foraging tree finished

--- src/main/java/org/example/models/Map/SecondaryMapComponents/ForagingCrop.java
+++ src/main/java/org/example/models/Map/SecondaryMapComponents/ForagingCrop.java
@@ -1,7 +1,24 @@
 package org.example.models.Map.SecondaryMapComponents;
 
-public class ForagingCrop {
+import org.example.models.Item;
+import org.example.models.Map.MapComponents;
+import org.example.models.Position;
+import org.example.models.enums.types.ForagingCropType;
+
+public class ForagingCrop extends MapComponents implements Item {
+
+    private final ForagingCropType type;
+
+    public ForagingCrop(Position position) {
+        super(position, 1, 1);
+
+        if (  )
+
+    }
 
 
+    @Override
+    public void update() {
 
+    }
 }
