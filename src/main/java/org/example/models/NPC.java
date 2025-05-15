1ca1ee476c7551f2532aba17884ccedb499330f3
amiryusofabdi <amiryusofabdi1@gmail.com>
2025-05-15 20:19:17 +0330

    npc4

--- src/main/java/org/example/models/NPC.java
+++ src/main/java/org/example/models/NPC.java
@@ -12,7 +12,7 @@ public class NPC extends Human {
     private  NPCType type;
     private  Position position;
     private  Role role;
-    private  ArrayList<String> dialog;
+    private  HashMap<String, Situation> dialog;
     private  HashMap<HashMap<Item, Integer>, HashMap<Item, Integer>> quests;
     private  ArrayList<Item> favorites;
 
@@ -20,20 +20,7 @@ public class NPC extends Human {
 
         super(friendshipLevel);
         this.type = type;
-
-        this.dialog = new ArrayList<>();
-        this.dialog.add("Hello there! Another beautiful day in the valley.");
-        this.dialog.add("Have you checked the crops today? They’re looking fantastic.");
-        this.dialog.add("I heard strange noises near the mines last night… Be careful.");
-        this.dialog.add("Summer’s here! Perfect weather for fishing.");
-        this.dialog.add("The town festival is coming up soon—I can’t wait!");
-        this.dialog.add("It’s been a long day… I think I’ll relax by the lake.");
-        this.dialog.add("Winter chills my bones. A warm cup of tea is all I need.");
-        this.dialog.add("Fall colors are breathtaking, don’t you think?");
-        this.dialog.add("You know, I always dreamt of being a famous musician. Life had other plans.");
-        this.dialog.add("Rainy days make me nostalgic… Something about the sound of raindrops.");
-        this.dialog.add("I found something interesting in the forest yesterday. Want to take a look?");
-        this.dialog.add("The fortune teller said today is a lucky day! Maybe I should go mining.");
+        this.dialog = type.getDialog();
 
     }
 
@@ -53,7 +40,7 @@ public class NPC extends Human {
         return role;
     }
 
-    public ArrayList<String> getDialog() {
+    public HashMap<String, Situation> getDialog() {
         return dialog;
     }
 
@@ -66,10 +53,6 @@ public class NPC extends Human {
 
     public ArrayList<Item> getFavorites() {
         return favorites;
-    }
-
-    public void addDialog(String sentence) {
-        this.dialog.add(sentence);
     }
 
     /*public void giveGift(Item item) {
