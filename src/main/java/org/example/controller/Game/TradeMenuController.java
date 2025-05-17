726c0444136f0ad6de8fa83dbabc43a91a84d2cb
amiryusofabdi <amiryusofabdi1@gmail.com>
2025-05-17 16:58:42 +0330

    trade

--- src/main/java/org/example/controller/Game/TradeMenuController.java
+++ src/main/java/org/example/controller/Game/TradeMenuController.java
@@ -4,28 +4,51 @@ import org.example.models.*;
 
 
 public class TradeMenuController {
+    public Result trade(String username, String typeStr, String itemName, String amountStr, String priceStr,
+                        String targetItemName, String targetAmountStr) {
+        User player = App.dataManager.getCurrentUser();
+        if (username.equals(player.getUsername())) {
+            return new Result(false, "You can't trade with yourself.");
+        }
 
-    public Result tradeWithMoney(String targetUsername, String type, String itemName, int amount, int price) { // type?
+        Game game = App.dataManager.getCurrentGame();
 
-//         Player targetPlayer =
+        User targetUser = game.getPlayerByUsername(username);
+        if (targetUser == null) {
+            return new Result(false, "User not found.");
+        }
 
-//        App.dataManager.getCurrentGame().map[x][y]
+        if (!amountStr.matches("\\d+")) {
+            return new Result(false, "Invalid amount.");
+        }
+        int amount = Integer.parseInt(amountStr);
 
+        // TODO: Item item = ...
+//        if (item == null) {
+//            return new Result(false, "Item not found.");
+//        }
+
+        boolean isOffer = typeStr.equals("offer");
+        if (priceStr != null) {
+            int price = Integer.parseInt(priceStr);
+            if (targetItemName != null) {
+                return new Result(false, "You can only trade with money or item and not both.");
+            }
+            // TODO
+        }
         return new Result(true, "");
     }
 
-    public Result tradeWithItem(String targetUsername, String type, String itemName, int amount, String targetItemName, int targetAmount) { // type?
-        // TODO: create a Trade class; int ID, User user1, User user2, Hashmap<Item, Integer>
+    public Result showTradeList() {
         return new Result(true, "");
     }
 
-    public Result showTradeList(){
+    public Result tradeResponse(String response, String idStr) {
         return new Result(true, "");
     }
 
-    public Result AorRtrade(){
+    public Result showTradeHistory() {
         return new Result(true, "");
+
     }
-
-
 }
